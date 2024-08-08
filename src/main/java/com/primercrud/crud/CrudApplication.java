d39b066f3e5f86a1e24256b589f2f2f6a78e125f
Seba <sebastiandanielgosti8@gmail.com>
2024-08-08 05:48:54 -0300

    primer proyecto

---
+++ src/main/java/com/primercrud/crud/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.primercrud.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
